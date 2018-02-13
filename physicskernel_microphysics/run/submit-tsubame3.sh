3b07bc74da21eec8dacd008685f2c0981057e87c
Michel Müller <michel@typhooncomputing.com>
2018-02-13 12:16:08 +0900

    implementing data region

--- physicskernel_microphysics/run/submit-tsubame3.sh
+++ physicskernel_microphysics/run/submit-tsubame3.sh
@@ -1,3 +1,3 @@
 arch=$1
 
-qsub -cwd -g tga-ASUCA -l f_node=1 -l h_rt=00:10:00 -N physicskernel_microphysics -o OUT.log -e ERR.log -v arch=${arch} run-tsubame3.sh
+qsub -cwd -g tga-ASUCA -l q_node=1 -l h_rt=00:05:00 -N physicskernel_microphysics -o OUT.log -e ERR.log -v arch=${arch} run-tsubame3.sh
